38aff4c8eb18932f138b626017171862afda740a
Usman Fayyaz <usmanf@gmail.com>
2024-06-08 20:06:44 +0100

    Creating Optional Constraints In Code (done)

---
+++ sample-code/layout-priorities/Priorities-v2My/Priorities-v2My/Priorities-v2My/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  Priorities-v2My
+//
+//  Created by Usman Fayyaz on 08/06/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+
+}
+
---
+++ sample-code/layout-priorities/Priorities-v2My/Priorities-v2My/Priorities-v2My/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  RootViewController.swift
+//  Priorities-v2My
+//
+//  Created by Usman Fayyaz on 08/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let sunView = UIImageView(named: "Sun", backgroundColor: .systemOrange)
+    
+    private let snowView = UIImageView(named: "Snow", backgroundColor: .systemBlue)
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 32)
+        label.text = "This label should be below the tallest of the two images"
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    private func setupView() {
+        view.addSubview(sunView)
+        view.addSubview(snowView)
+        view.addSubview(captionLabel)
+        
+        let margins = view.layoutMarginsGuide
+
+        let captionTopConstraint = captionLabel.topAnchor.constraint(equalTo: margins.topAnchor)
+        captionTopConstraint.priority = .defaultLow
+
+        NSLayoutConstraint.activate([
+            sunView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            sunView.topAnchor.constraint(equalTo: margins.topAnchor),
+            snowView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            snowView.topAnchor.constraint(equalTo: margins.topAnchor),
+            captionLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            captionLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            
+            ///
+            captionLabel.topAnchor.constraint(greaterThanOrEqualToSystemSpacingBelow: sunView.bottomAnchor, multiplier: 1.0),
+            captionLabel.topAnchor.constraint(greaterThanOrEqualToSystemSpacingBelow: snowView.bottomAnchor, multiplier: 1.0),
+            
+            captionTopConstraint
+        ])
+        
+    }
+    
+    
+}
+
+
+private extension UIImageView {
+    convenience init(named name: String, backgroundColor: UIColor) {
+        self.init(image: UIImage(named: name))
+        self.backgroundColor = backgroundColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
