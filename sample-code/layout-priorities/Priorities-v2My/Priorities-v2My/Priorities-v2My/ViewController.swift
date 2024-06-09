529c6f1513d1b065db7dfc8568971a1fc2a3a2e2
Usman Fayyaz <usmanf@gmail.com>
2024-06-09 13:10:40 +0100

    Playing with minimum number of constraints required when there is only one label in your view hairarchy

--- sample-code/layout-priorities/Priorities-v2My/Priorities-v2My/Priorities-v2My/ViewController.swift
+++ sample-code/layout-priorities/Priorities-v2My/Priorities-v2My/Priorities-v2My/ViewController.swift
@@ -29,28 +29,38 @@ class ViewController: UIViewController {
     }
     
     private func setupView() {
-        view.addSubview(sunView)
-        view.addSubview(snowView)
+//        view.addSubview(sunView)
+//        view.addSubview(snowView)
         view.addSubview(captionLabel)
         
         let margins = view.layoutMarginsGuide
 
         let captionTopConstraint = captionLabel.topAnchor.constraint(equalTo: margins.topAnchor)
         captionTopConstraint.priority = .defaultLow
+        
+        
+        let captionBottomConstraint = captionLabel.bottomAnchor.constraint(lessThanOrEqualToSystemSpacingBelow: margins.bottomAnchor, multiplier: 1)
+        captionBottomConstraint.priority = .defaultLow
+        
 
         NSLayoutConstraint.activate([
-            sunView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
-            sunView.topAnchor.constraint(equalTo: margins.topAnchor),
-            snowView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
-            snowView.topAnchor.constraint(equalTo: margins.topAnchor),
+//            sunView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+//            sunView.topAnchor.constraint(equalTo: margins.topAnchor),
+//            snowView.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+//            snowView.topAnchor.constraint(equalTo: margins.topAnchor),
+            
             captionLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
             captionLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor),
+            captionLabel.topAnchor.constraint(equalTo: margins.topAnchor),
+//            captionLabel.bottomAnchor.constraint(lessThanOrEqualToSystemSpacingBelow: margins.bottomAnchor, multiplier: 1),
+            
+            captionBottomConstraint,
             
             ///
-            captionLabel.topAnchor.constraint(greaterThanOrEqualToSystemSpacingBelow: sunView.bottomAnchor, multiplier: 1.0),
-            captionLabel.topAnchor.constraint(greaterThanOrEqualToSystemSpacingBelow: snowView.bottomAnchor, multiplier: 1.0),
+//            captionLabel.topAnchor.constraint(greaterThanOrEqualToSystemSpacingBelow: sunView.bottomAnchor, multiplier: 1.0),
+//            captionLabel.topAnchor.constraint(greaterThanOrEqualToSystemSpacingBelow: snowView.bottomAnchor, multiplier: 1.0),
             
-            captionTopConstraint
+//            captionTopConstraint
         ])
         
     }
